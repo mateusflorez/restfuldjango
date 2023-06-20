1b99695e388b52ef03ecf8100467eac20defb2ea
Mateus Paz <54851249+mateusflorez@users.noreply.github.com>
2023-06-20 15:41:09 -0300

    student registrations route

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from escola.models import Aluno, Curso, Matricula
-from .serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer
+from .serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer
 
 class AlunosViewSet(viewsets.ModelViewSet):
     """Lista alunos"""
@@ -16,3 +16,10 @@ class MatriculasViewSet(viewsets.ModelViewSet):
     """Lista matriculas"""
     queryset = Matricula.objects.all()
     serializer_class = MatriculaSerializer
+
+class ListaMatriculasAluno(generics.ListAPIView):
+    """Lista matriculas de aluno"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(aluno_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasAlunoSerializer
